b05d21a3292187f95e5f3fb95b29b0fe9225779f
Mikan-777-star <79134261+Mikan-777-star@users.noreply.github.com>
2021-03-06 21:12:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((i, i1) =>{
     i1.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
     i1.write(i.headers['user-agent']);
-    i.end();
+    i1.end();
 })
 const port = 8000;
 server.listen(port, () => console.log('Listening on '+ port));
