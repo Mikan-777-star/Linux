0a1c3dd5fe806ef8467579436e13775238a5c66f
Mikan-777-star <79134261+Mikan-777-star@users.noreply.github.com>
2021-03-06 21:30:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const http = require('http');
 const server = http.createServer((i, i1) =>{
     i1.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    i1.write(i.headers['<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>']);
+    i1.write('<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>');
     i1.end();
 })
 const port = 8000;
