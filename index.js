e6f5a785fba1f245384f8f0fb401dcb364f65020
Mikan-777-star <79134261+Mikan-777-star@users.noreply.github.com>
2021-03-06 21:27:33 +0900

    ナニコレ

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((i, i1) =>{
-    i1.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-    i1.write(i.headers['user-agent']);
+    i1.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    i1.write(i.headers['<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>']);
     i1.end();
 })
 const port = 8000;
