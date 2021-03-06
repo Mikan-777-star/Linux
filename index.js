c065cd22252b73358718b2d434c9c1939bf6b78b
Mikan-777-star <79134261+Mikan-777-star@users.noreply.github.com>
2021-03-06 21:10:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-import http from 'http';
+const http = require('http');
 const server = http.createServer((i, i1) =>{
     i1.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
     i1.write(i.headers['user-agent']);
