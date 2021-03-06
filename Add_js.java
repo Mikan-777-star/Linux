c63094403c7cdb023bd7cfa326b43245446cc101
Mikan-777-star <79134261+Mikan-777-star@users.noreply.github.com>
2021-03-06 21:06:16 +0900

    すごいいろいろ

--- Add_js.java
+++ Add_js.java
@@ -4,17 +4,11 @@ import java.io.OutputStream;
 
 public class Add_js{
     public static void main(String[] args) {
-        if(args.length < 1){
-            System.out.println("コマンドラインでファイル名を指定してください");
-            System.exit(-1);
-        }
-        File f1 = new File(args[0]);
-        if(f1.exists()){
-            System.out.println("そのファイルは既に存在します");
-            System.exit(-1);
-        }
+        File f1 =null;
+        int i = 1;
+        while((f1 = new File("Test"+i+".js")).exists()) i++;
         try (OutputStream os = new FileOutputStream(f1)) {
-            os.write("\'use strict\'".getBytes());
+            os.write("\'use strict\';\n".getBytes());
             os.flush();
         } catch (Exception e) {
             e.printStackTrace();
---
+++ Test4.js
@@ -0,0 +1,9 @@
+'use strict';
+import http from 'http';
+const server = http.createServer((i, i1) =>{
+    i1.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+    i1.write(i.headers['user-agent']);
+    i.end();
+})
+const port = 8000;
+server.listen(port, () => console.log('Listening on '+ port));
