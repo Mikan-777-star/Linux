a584c73c167290a68f6b010d3865b2b4aafd262a
Mikan-777-star <79134261+Mikan-777-star@users.noreply.github.com>
2021-03-10 08:42:44 +0900

    Update Copy.java

--- Copy.java
+++ Copy.java
@@ -2,7 +2,7 @@ import java.io.File;
 
 public class Copy{
     public static void main(String[] args) {
-        File file = new File("~/workspace/node-js-http-3013/"+System.console().readLine());
+        File file = new File("home/workspace/node-js-http-3013/"+System.console().readLine());
         System.out.println(file.exists());
     }
 }
