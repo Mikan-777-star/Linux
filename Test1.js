80ef37ea495fc780392f52dcb3fff0be08b12620
Mikan-777-star <79134261+Mikan-777-star@users.noreply.github.com>
2021-03-15 13:39:44 +0900

    Typescript
    なんであれでエラー吐いちゃうの？

--- Test1.js
+++ Test1.js
@@ -1,2 +1,7 @@
-'use strict'
-console.log("Hello World of javascript!");
+'use strict';
+module.exports = function (r) {
+    r.hear(/hello>i/, function (msg) {
+        var user_id = msg.message.user.id;
+        msg.send("Hello,<@" + user_id + ">.\n Today is " + new Date());
+    });
+};
