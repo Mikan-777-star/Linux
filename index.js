f99f0eec3f1382ba6e2d788eb950ad0bec823a66
Mikan-777-star <79134261+Mikan-777-star@users.noreply.github.com>
2021-03-16 10:42:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 const http = require('http');
 const server = http.createServer((req, res) =>{
     console.log(new Date());
+    console.log(req);
     res.writeHead(200, {
-        'Content-Type': 'text/plain; charset=utf-8'
+        'Content-Type': 'text/html; charset=utf-8'
     });
     const rl = require('readline').createInterface({input: require('fs').createReadStream('Test1.txt'),output:{}});
-    let s = ''
-    rl.on('line', line => s += line + '\n');
-    res.write(s);
+    let s = '<h1>'
+    rl.on('line', line => s += line + '<br>');
+    res.write(s + '</h1>');
     res.end();
 });
 server.listen(8000, () => {
