751ac1da2c14570889093cc7f4d2cad2838c461a
Mikan-777-star <79134261+Mikan-777-star@users.noreply.github.com>
2021-03-16 12:24:28 +0900

    これでいけるかのテスト

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 'use strict';
 
 const http = require('http');
+const rl = require('readline').createInterface({input: require('fs').createReadStream('Test1.txt'),output:{}});
+var s = "";
+rl.on('line', line => s += line + "\n")
+//  .on('close',() => console.log(s));
 const server = http.createServer((req, res) =>{
     console.log(new Date());
     console.log(req);
     res.writeHead(200, {
         'Content-Type': 'text/html; charset=utf-8'
     });
-    const rl = require('readline').createInterface({input: require('fs').createReadStream('./Test1.txt'),output:{}});
-    let s = '<h1>'
-    rl.on('line', line => s += line + '<br>');
-    res.write(s + '</h1>');
+    res.write('<h1>' + s + '</h1>');
     res.end();
 });
 server.listen(8000, () => {
