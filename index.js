840799d7d1e18b8eb9a0bf000f11659d294dc402
Mikan-777-star <79134261+Mikan-777-star@users.noreply.github.com>
2021-03-16 16:06:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,28 @@ rl.on('line', line => s += line + "<br>")
   .on('close',() => console.log(s));
 const server = http.createServer((req, res) =>{
     console.log(new Date() + 'Req by' + req.connection.remoteAddress);
-//    console.log(req);
     res.writeHead(200, {
         'Content-Type': 'text/html; charset=utf-8'
     });
-    res.write('<h3>' + s + '</h3>');
+//    console.log(req);
+    switch(req.method){
+        case 'GET' : {
+            res.write('<h3>' + s + '</h3>');
+        }
+        break;
+        case 'POST' : {
+            let data = '';
+            req
+            .on('data', get => data += get)
+            .on('end', () => console.log('[' + new Date() + '] Data posted: ' + get));
+        }
+        break;
+        default:{}
+        break;
+    }
     res.end();
-});
+})
+.on('error', e =>console.error(new Date() + " Server error " , e));
 server.listen(8000, () => {
     console.log("server on");
 })
