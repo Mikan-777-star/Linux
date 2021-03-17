0af6741d50c05779c7cd2ce96747558b6d8b9e45
Mikan-777-star <79134261+Mikan-777-star@users.noreply.github.com>
2021-03-17 18:10:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   const now = Date.now();
-  res.setHeader('Set-Cookie', 'last_access = '+ now + '; Test1 = もんもぉ！;');
+  res.setHeader('Set-Cookie', 'last_access = '+ now + ', Test1 = もんもぉ！;');
   const last_access_time = req.headers.cookie ? parseInt(req.headers.cookie.split('last_access=')[1]) : now;
   const monmo = req.headers.cookie ? req.headers.cookie.split("Test1=")[1] : "シャミ子";
   res.end(new Date(last_access_time).toString());
