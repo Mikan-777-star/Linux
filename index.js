523cc7e8d28b536d13b799cec4873f8f0873109a
Mikan-777-star <79134261+Mikan-777-star@users.noreply.github.com>
2021-03-17 17:22:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,9 @@
 'use strict';
 const http = require('http');
-const server = http
-  .createServer((req, res) => {
-    const now = new Date();
-    console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    });
-
-    switch (req.method) {
-      case 'GET':
-        const fs = require('fs');
-        const rs = fs.createReadStream('./Test1.html');
-        rs.pipe(res);
-        break;
-      case 'POST':
-        let rawData = '';
-        req
-          .on('data', chunk => {
-            rawData = rawData + chunk;
-          })
-          .on('end', () => {
-            const decoded = decodeURIComponent(rawData);
-            console.info('[' + now + '] 投稿: ' + decoded);
-            res.write(
-              '<!DOCTYPE html><html lang="ja"><body><h1>' +
-                decoded +
-                'が投稿されました</h1></body></html>'
-            );
-            res.end();
-          });
-        break;
-      default:
-        break;
-    }
-  })
-  .on('error', e => {
-    console.error('[' + new Date() + '] Server Error', e);
-  })
-  .on('clientError', e => {
-    console.error('[' + new Date() + '] Client Error', e);
-  });
+const server = http.createServer((req, res) => {
+  res.end('hi');
+});
 const port = 8000;
 server.listen(port, () => {
-  console.info('[' + new Date() + '] Listening on ' + port);
+  console.info('Listening on ' + port);
 });
