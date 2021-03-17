d6b23d06dcc87b3b81787750415a755cc0f44083
Mikan-777-star <79134261+Mikan-777-star@users.noreply.github.com>
2021-03-17 11:51:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
 'use strict';
-var s = "";
-const rl = require('readline').createInterface({input: require('fs').createReadStream('./Test1.html'),output:{}});
-rl.on('line', line => s += line + "\n");
 const http = require('http');
-const server = http.createServer((req, res) =>{
-    console.log(new Date() + 'Req by' + req.connection.remoteAddress);
+const server = http
+  .createServer((req, res) => {
+    const now = new Date();
+    console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
     res.writeHead(200, {
-        'Content-Type': 'text/html; charset=utf-8'
+      'Content-Type': 'text/html; charset=utf-8'
     });
-//    console.log(req);
-    switch(req.method){
-        case 'GET' : res.write(s);
+
+    switch (req.method) {
+      case 'GET':
+        const fs = require('fs');
+        const rs = fs.createReadStream('./form.html');
+        rs.pipe(res);
         break;
-        case 'POST' : {
-            let data = '';
-            req
-            .on('data', get => data = get + data)
-            .on('end', () => {
-                const decoded = decodeURIComponent(data);
-                console.info('[' + new Date() + '] 投稿: ' + decoded);
-                res.write('<!DOCTYPE html><html lang="ja"><body><h1>' + decoded + 'が投稿されました</h1></body></html>', e => console.error("なんかのエラー" + e));
-            });
+      case 'POST':
+        let rawData = '';
+        req
+          .on('data', chunk => {
+            rawData = rawData + chunk;
+          })
+          .on('end', () => {
+            const decoded = decodeURIComponent(rawData);
+            console.info('[' + now + '] 投稿: ' + decoded);
+            res.write(
+              '<!DOCTYPE html><html lang="ja"><body><h1>' +
+                decoded +
+                'が投稿されました</h1></body></html>'
+            );
             res.end();
-        }
+          });
         break;
-        default:{res.write("<h1>ミカンママのおっぱい！<br>えっち！！</h1>")}
+      default:
         break;
     }
-})
-.on('error', e =>console.error(new Date() + " Server error " , e))
-.on('clientError', e => console.error('[' + new Date() + '] Client Error', e));
-server.listen(8000, () => {
-    console.log("server on");
-})
+  })
+  .on('error', e => {
+    console.error('[' + new Date() + '] Server Error', e);
+  })
+  .on('clientError', e => {
+    console.error('[' + new Date() + '] Client Error', e);
+  });
+const port = 8000;
+server.listen(port, () => {
+  console.info('[' + new Date() + '] Listening on ' + port);
+});
