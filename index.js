ffe2189175b82b19a08e2c343c57a8fc57d4158a
Mikan-777-star <79134261+Mikan-777-star@users.noreply.github.com>
2021-03-17 17:37:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  const now = Date.now();
+  res.setHeader('Set-Cookie', 'last_access = '+ now + ';');
   res.end('hi');
 });
 const port = 8000;
 server.listen(port, () => {
-  const now = Date.now();
-  res.setHeader('Set-Cookie', 'last_access = '+ now + ';');
+
   console.info('Listening on ' + port);
 });
