9b2cc2232e2a72467183ea0c3fec149260c929e8
Mikan-777-star <79134261+Mikan-777-star@users.noreply.github.com>
2021-03-17 18:22:37 +0900

    ミカンママァァァァァ

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-  const now = Date.now();
-  res.setHeader('Set-Cookie', 'last_access = '+ now + ', Test1 = もんもぉ！;');
-  const last_access_time = req.headers.cookie ? parseInt(req.headers.cookie.split('last_access=')[1]) : now;
-  const monmo = req.headers.cookie ? req.headers.cookie.split("Test1=")[1] : "シャミ子";
-  res.end(new Date(last_access_time).toString());
-  console.log(monmo);
+  res.writeHead(302, {
+    Location: 'https://mikan-777-star.github.io/Mikan_1/'
+  });
+  res.end();
 });
 const port = 8000;
 server.listen(port, () => {
