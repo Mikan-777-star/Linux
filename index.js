121748ea45c3680b4ab67b1c6e927b527b9f8f70
Mikan-777-star <79134261+Mikan-777-star@users.noreply.github.com>
2021-03-17 11:27:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const server = http.createServer((req, res) =>{
             .on('end', () => {
                 const decoded = decodeURIComponent(data);
                 console.info('[' + new Date() + '] 投稿: ' + decoded);
-                res.write('<!DOCTYPE html><html lang="ja"><body><h1>' + decoded + 'が投稿されました</h1></body></html>');
+                res.write('<!DOCTYPE html><html lang="ja"><body><h1>' + decoded + 'が投稿されました</h1></body></html>', e => console.error("なんかのエラー" + e));
             });
         }
         break;
