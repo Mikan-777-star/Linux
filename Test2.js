f5fd5992630c524405b80e8cfefe153091488880
Mikan-777-star <79134261+Mikan-777-star@users.noreply.github.com>
2021-03-30 23:29:31 +0900

    m
    moutukareta

--- Test2.js
+++ Test2.js
@@ -1,6 +1,10 @@
 'use strict';
-
 const rl = require('readline').createInterface({input: require('fs').createReadStream('Test1.txt'),output:{}});
+const ws = require('fs').createWriteStream("Test2.txt");
 var s = "";
-rl.on('line', line => s += line + "\n")
-  .on('close',() => console.log(s));
+rl
+  .on('line', line => s += line + "\n")
+  .on('close',() => {
+    ws.write(s, e => console.log(e));
+    ws.close();
+  });
